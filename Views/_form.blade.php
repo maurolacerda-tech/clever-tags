c3c7e5441c5a13fab0a018fb352cfcc438c9a1cf
maurolacerda-tech <contato@maurolacerda.com.br>
2020-09-04 12:25:31 -0300

    ajuste nos nomes dos campos do formulario

--- Views/_form.blade.php
+++ Views/_form.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group">
-            {{ Form::label('name', $combine_filds['name']) }}
+            {{ Form::label('name', 'Nome') }}
             {{ Form::text('name', null, ['class' => $errors->has('name') ?  'form-control is-invalid' : 'form-control']) }}
             @include('admin.partials._help_block',['field' => 'name'])
         </div>
@@ -12,7 +12,7 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group">
-            {{ Form::label('email', $combine_filds['email']) }}
+            {{ Form::label('email', 'E-mail') }}
             {{ Form::email('email', null, ['class' => $errors->has('email') ?  'form-control is-invalid' : 'form-control']) }}
             @include('admin.partials._help_block',['field' => 'email'])
         </div>
@@ -21,7 +21,7 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group">
-            {{ Form::label('phone', $combine_filds['phone']) }}
+            {{ Form::label('phone', 'Telefone') }}
             {{ Form::text('phone', null, ['class' => $errors->has('phone') ?  'form-control is-invalid' : 'form-control']) }}
             @include('admin.partials._help_block',['field' => 'phone'])
         </div>
